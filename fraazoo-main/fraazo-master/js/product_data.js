d3ba42d0a89d874e693ed87d83fb3ddcc6bf2e9a
Ayush Anshu <ayushanshu56@gmail.com>
2022-05-05 19:19:48 +0530

    product

---
+++ fraazoo-main/fraazo-master/js/product_data.js
@@ -0,0 +1,246 @@
+var veggie = [
+    {
+        name: "Carrot Orange",
+        src: "https://i.postimg.cc/rmYTMXZF/carrot.png",
+        price: "30",
+        weight: "300 gm"
+    },
+
+    {
+        name: "Sweet Corn",
+        src: "https://i.postimg.cc/pLxL0vgg/sweet-corn.png",
+        price: "14",
+        weight: "1 pc"
+    },
+
+    {
+        name: "Cabbage Red",
+        src: "https://i.postimg.cc/HkQT5NnB/cabbage-red.png",
+        price: "37",
+        weight: "1 pc"
+    },
+
+    {
+        name: "Basil Italian",
+        src: "https://i.postimg.cc/PxffdMtK/basil-italian.png",
+        price: "11",
+        weight: "50 gm"
+    },
+
+    {
+        name:"Mushroom",
+        src: "https://i.postimg.cc/XJXM21NM/mushroom.png",
+        price: "37",
+        weight: "1 unit"
+    },
+
+    {
+        name: "Tamoto Cherry",
+        src: "https://i.postimg.cc/TP3Y6920/tamoto-cherry.png",
+        price: "45",
+        weight: "200 gm"
+    },
+
+    {
+        name: "Saver PT Combo",
+        src: "https://i.postimg.cc/85FpFz6r/saver-pt-combo.png",
+        price: "68",
+        weight: "1+1 kg"
+    },
+
+
+    {
+        name: "Saver PO Combo",
+        src: "https://i.postimg.cc/bY7JtkLY/saver-po-combo.png",
+        price: "49",
+        weight: "1+1 kg"
+    },
+
+
+    {
+        name: "3 kg POT Combo",
+        src: "https://i.postimg.cc/jdBjyNXd/3kg-pot-combo.png",
+        price: "99",
+        weight: "1+1+1 kg"
+    },
+
+    {
+        name: "Beans Flat",
+        src: "https://i.postimg.cc/vTkGLF5S/beans-flat.png",
+        price: "25",
+        weight: "250 gm"
+    },
+
+    {
+        name: "Green Pea",
+        src: "https://i.postimg.cc/KYGhTvd8/green-pea.png",
+        price: "30",
+        weight: "500 gm"
+    },
+
+    {
+        name: "Beans Chawli",
+        src: "https://i.postimg.cc/SKhNvh3B/beans-chawli.png",
+        price: "25",
+        weight: "250 gm"
+    }
+];
+
+var hydro = [
+    {
+        src:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/169/original/data?1599847670",
+        name:"Celery",
+        weight:"15-200Gm",
+        price:"107 "
+    },{
+        src:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/631/original/data?1635766646",
+        name:"Mixed lettuice",
+        weight:"1 punnet",
+        price:"69"
+    },{
+        src:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/003/103/original/data?1639301565",
+        name:"Kale Curly",
+        weight:"200Gm",
+        price:"129"
+    }
+];
+
+var fruits = [
+    {
+        src:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FBAN14.png",
+        name:"Banana Robusta",
+        weight:"1 kg",
+        price:"29"
+
+    },{
+        src:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FBAN24.png",
+        name:"Banana Elaichi",
+        weight:"1 kg",
+        price:"39"
+    },{
+        src:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FCHI13.png",
+        name:"Chikoo",
+        weight:"1 kg",
+        price:"49"
+    },{
+        src:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FGRA42.png",
+        name:"Grapes Green",
+        weight:"1 kg",
+        price:"70"  
+    },{
+        src:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/233/original/data?1609860341",
+        name:"Grapes Black",
+        weight:"1 kg",
+        price:"90"  
+    },{
+        src:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/369/original/data?1622631944",
+        name:"Guava White",
+        weight:"1 kg",
+        price:"110"  
+    },{
+        src:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/395/original/data?1625917064",
+        name:"Baby Orange",
+        weight:"1 kg",
+        price:"90"  
+    },{
+        src:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/261/original/data?1612541635",
+        name:"Pomegranate",
+        weight:"1 kg",
+        price:"80" 
+    }
+];
+
+var herbs=[
+    {
+        src:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/419/original/data?1635147438",
+        name:"Colocasia Leaves",
+        weight:"1pcs",
+        price:"4"
+    },{
+        src:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/602/original/data?1635155510",
+        name:"Betel Leafs",
+        weight:"12 pcs",
+        price:"21"
+    },{
+        src:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/127/original/data?1603884053",
+        name:"Masala Combo",
+        weight:"12 Pkt",
+        price:"13"
+    }
+];
+
+var dryFruits = [
+    {
+        name: "Cashew - 100gm",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/577/original/data?1635085485",
+        price: "99",
+        weight: "100 gms"
+    },
+
+    {
+        name: "Cashew Tukda - 100gm",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/580/original/data?1635086672",
+        price: "92",
+        weight: "100 gms"
+    },
+
+    {
+        name: "Walnut - 100gms",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/583/original/data?1635137039",
+        price: "175",
+        weight: "100 gms"
+    },
+
+    {
+        name: "Pista Salted - 100gms",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/586/original/data?1635141850",
+        price: "122",
+        weight: "100 gms"
+    },
+
+    {
+        name: "Pista Kernel - 100gms",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/588/original/data?1635142184",
+        price: "186",
+        weight: "100 gms"
+    },
+
+    {
+        name: "Almond - 100gms",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/590/original/data?1635143515",
+        price: "109",
+        weight: "100 gms"
+    },
+
+    {
+        name: "Kismis - 200 gms",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/593/original/data?1635144647",
+        price: "68",
+        weight: "200 gms"
+    },
+
+
+    {
+        name: "Dry Dates - 400 gms",
+        src: "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/597/original/data?1635146300",
+        price: "135",
+        weight: "400 gms"
+    }
+];
+
+var dairy = [
+    {
+      src:
+        "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/394/original/data?1625864720",
+      name: "ISKCON's A2 Cream Ghee",
+      weight: "225ml",
+      price: "375",
+    },
+    {
+      src:
+        "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/367/original/data?1622452513",
+      name: "ISKCON's A2 Bilano Ghee",
+      weight: "500ml",
+      price: "1250",
+    },
+];
